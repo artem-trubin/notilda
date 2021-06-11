992e0c70fba441d69ec11227dd042e3383841050
yaripey <17trubin@gmail.com>
2021-06-11 23:52:07 +0300

    deleted old_src

--- src_old/components/Announcer.js
+++
@@ -1,9 +0,0 @@
-import styles from './main.css'
-
-const Announcer = () => `
-    <div class="${styles.announcerText}">
-        ${window.state.announcerText}
-    </div>
-`
-
-export default Announcer
