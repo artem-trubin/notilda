8f1dfcf523544ec91a21134871134f765efda8b8
yaripey <17trubin@gmail.com>
2021-06-12 15:13:44 +0300

    added handling for empty list of notes

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -7,22 +7,24 @@ const NoteList = ({ isDataLoading, notes, editingNoteID, updateNote, deleteNote,
   if (isDataLoading) {
     return <div>Data is loading</div>;
   } else {
-    return (
-      <ul className={StyleSheet.noteList}>
-        {notes
-          .map(note => (
-            <Note
-              key={note.id}
-              note={note}
-              isEditing={editingNoteID === note.id}
-              updateNote={updateNote}
-              editNote={editNote}
-              deleteNote={deleteNote}
-            />
-          ))
-          .reverse()}
-      </ul>
-    );
+    if (notes.length > 0)
+      return (
+        <ul className={StyleSheet.noteList}>
+          {notes
+            .map(note => (
+              <Note
+                key={note.id}
+                note={note}
+                isEditing={editingNoteID === note.id}
+                updateNote={updateNote}
+                editNote={editNote}
+                deleteNote={deleteNote}
+              />
+            ))
+            .reverse()}
+        </ul>
+      );
+    else return <div>Create your first note by pressing "Add note".</div>;
   }
 };
 
